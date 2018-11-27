2480b95131b7a116361d2ae14e10e91ff7ee3d95
bagusbimo <bagus.b@mail.ugm.ac.id>
2018-11-27 11:00:51 +0700

    RGB Setting

--- src/components/setting.vue
+++ src/components/setting.vue
@@ -12,7 +12,8 @@
                     <v-layout justify-center>
                         <div class="headline">JADWAL PENYIRAMAN 1</div>
                     </v-layout>
-                </v-card-title>   
+                </v-card-title>
+                <br>   
                 <v-layout row mx-3>
                     <div>
                         <v-icon color="pink" size="48" center>alarm</v-icon>
@@ -48,7 +49,8 @@
                     <v-layout justify-center>
                         <div class="headline">JADWAL PENYIRAMAN 2</div>
                     </v-layout>
-                </v-card-title>   
+                </v-card-title>
+                <br>   
                 <v-layout row mx-3>
                     <div>
                         <v-icon color="pink" size="48" center>alarm</v-icon>
@@ -78,61 +80,77 @@
               </v-card>
             </v-flex>
 
-            <v-flex xs12>
+            <v-flex>
               <v-card color="white" class="pink--text">
                 <v-card-title primary-title>
-                  <div>
-                    <div class="headline">ATUR JADWAL NYALA LAMPU UV</div>
-                    <br>
-                    <br>
-                    <v-text-field
-                      v-model="jam3"
-                      type="number"
-                      label="Hour"
-                      placeholder="Jam"
-                      solo
-                    ></v-text-field>
-                    <v-text-field
-                      v-model="menit3"
-                      type="number"
-                      label="Minute"
-                      placeholder="Menit"
-                      solo
-                    ></v-text-field>
-                  </div>
+                    <v-layout justify-center>
+                        <div class="headline">ATUR JADWAL NYALA LAMPU UV</div>
+                    </v-layout>
                 </v-card-title>
-                <v-card-actions>
-                  <v-btn flat color="pink" @click="uvOn(pot)">Atur Jadwal</v-btn>
-                </v-card-actions>
+                <br>   
+                <v-layout row mx-3>
+                    <div>
+                        <v-icon color="pink" size="48" center>alarm</v-icon>
+                    </div>
+                    <v-layout row mx-1></v-layout>
+                    <v-text-field
+                        v-model="jam3"
+                        type="number"
+                        label="Hour"
+                        :placeholder = "pot[0].LampON.hour"
+                        solo
+                    ></v-text-field>
+                    <v-layout row mx-1></v-layout>
+                    <v-text-field
+                        v-model="menit3"
+                        type="number"
+                        label="Minute"
+                        :placeholder = "pot[0].LampON.min"
+                        solo
+                    ></v-text-field>
+                </v-layout>  
+                <v-layout justify-center>
+                    <v-btn color="primary" dark @click="uvOn(pot)">simpan
+                        <v-icon dark right>check_circle</v-icon>
+                    </v-btn>
+                </v-layout>
               </v-card>
             </v-flex>
 
-            <v-flex xs12>
+            <v-flex>
               <v-card color="white" class="pink--text">
                 <v-card-title primary-title>
-                  <div>
-                    <div class="headline">ATUR JADWAL MATI LAMPU UV</div>
-                    <br>
-                    <br>
-                    <v-text-field
-                      v-model="jam4"
-                      type="number"
-                      label="Hour"
-                      placeholder="Jam"
-                      solo
-                    ></v-text-field>
-                    <v-text-field
-                      v-model="menit4"
-                      type="number"
-                      label="Minute"
-                      placeholder="Menit"
-                      solo
-                    ></v-text-field>
-                  </div>
+                    <v-layout justify-center>
+                        <div class="headline">ATUR JADWAL MATI LAMPU UV</div>
+                    </v-layout>
                 </v-card-title>
-                <v-card-actions>
-                  <v-btn flat color="pink" @click="uvOff(pot)">Atur Jadwal</v-btn>
-                </v-card-actions>
+                <br>   
+                <v-layout row mx-3>
+                    <div>
+                        <v-icon color="pink" size="48" center>alarm</v-icon>
+                    </div>
+                    <v-layout row mx-1></v-layout>
+                    <v-text-field
+                        v-model="jam4"
+                        type="number"
+                        label="Hour"
+                        :placeholder = "pot[0].LampOFF.hour"
+                        solo
+                    ></v-text-field>
+                    <v-layout row mx-1></v-layout>
+                    <v-text-field
+                        v-model="menit4"
+                        type="number"
+                        label="Minute"
+                        :placeholder = "pot[0].LampOFF.min"
+                        solo
+                    ></v-text-field>
+                </v-layout>  
+                <v-layout justify-center>
+                    <v-btn color="primary" dark @click="uvOff(pot)">simpan
+                        <v-icon dark right>check_circle</v-icon>
+                    </v-btn>
+                </v-layout>
               </v-card>
             </v-flex>
 
@@ -145,13 +163,16 @@
                     <br>
                     <v-select
                     :items="items"
+                    item-text="warna"
+                    item-value="id"
                     label="Pilih Preset Warna"
+                    v-model="warnaa"
                     solo
                     ></v-select>
                   </div>
                 </v-card-title>
                 <v-card-actions>
-                  <v-btn flat color="pink">Atur Jadwal</v-btn>
+                  <v-btn flat color="pink" @click="rgbSet(pot)">Atur Jadwal</v-btn>
                 </v-card-actions>
               </v-card>
             </v-flex>
@@ -180,7 +201,58 @@ import potRef from './../config';
         menit4: '',
         newJam: '',
         newMenit: '',
-        items: ['Merah', 'Hijau', 'Biru', 'Kuning', 'Ungu', 'Putih']
+        items: [
+          {
+            warna: 'Merah',
+            r: 255,
+            g: 0,
+            b: 0,
+            id: 0,
+          },
+          {
+            warna: 'Hijau',
+            r: 0,
+            g: 255,
+            b: 0,
+            id: 1
+          },
+          {
+            warna: 'Biru',
+            r: 0,
+            g: 0,
+            b: 255,
+            id: 2
+          },
+          {
+            warna: 'Ungu',
+            r: 255,
+            g: 0,
+            b: 255,
+            id: 3
+          },
+          {
+            warna: 'Kuning',
+            r: 255,
+            g: 255,
+            b: 0,
+            id: 4
+          },
+          {
+            warna: 'Cyan',
+            r: 0,
+            g: 255,
+            b: 255,
+            id: 5
+          },
+          {
+            warna: 'Putih',
+            r: 255,
+            g: 255,
+            b: 255,
+            id: 6
+          }
+        ],
+        warnaa: null
       }
     },
     methods: {
@@ -215,6 +287,11 @@ import potRef from './../config';
         potRef.child('pot1').child('LampOFF').child('min').set(newMenit);
         this.jam4 = '';
         this.menit4 = '';
+      },
+      rgbSet: function(rgb) {
+        potRef.child('pot1').child('r').set(this.items[parseInt(this.warnaa)].r);
+        potRef.child('pot1').child('g').set(this.items[parseInt(this.warnaa)].g);
+        potRef.child('pot1').child('b').set(this.items[parseInt(this.warnaa)].b);
       }
     }
   }
